93d67e85607d0acb26263be99e09a83c2c7b8404
Saman Kumara <samankumararajapaksha660@gmail.com>
2023-08-07 15:08:08 +0530

    complete navigation bar

--- admin/store.php
+++ admin/store.php
@@ -5,19 +5,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Home</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-</head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" 
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+    </head>
 <body>
    
 <!-- nav bar -->
-<nav class="navbar bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand">SKstore</a>
-    <form class="d-flex" role="search">
-      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success" type="submit">Search</button>
-    </form>
+<nav class="navbar  bg-dark">
+  <div class="container-fluid text-white">
+    <a class="navbar-brand text-white">SKStore</a>
+    <span>
+    <i class="fa-solid fa-user-shield"></i>
+    Hello, |
+    <i class="fa-solid fa-right-from-bracket"></i>
+    <a href="" class="text-decoration-none text-white">Logout</a> |
+    <a href="" class="text-decoration-none text-white">UserPage</a> 
+    </span>
   </div>
 </nav>
+
+
 
 </body>
 </html>
