a585fadb35641a447208203981408d1d429556d9
Saman Kumara <samankumararajapaksha660@gmail.com>
2023-08-09 23:32:59 +0530

    modified

--- user/index.php
+++ user/index.php
@@ -28,15 +28,19 @@
 
     echo "
     <div class='col-md-6 col-lg-4 m-auto mb-3'>
+    <form action = 'Insertcart.php' method = 'POST'>
        <div class='card m-auto' style='width: 18rem;'>
     <img src='../admin/product/$row[PImage]' class='card-img-top m-auto' style = 'width : 200px; height:200px'>
     <div class='card-body text-center'>
       <h5 class='card-title text-danger fs-4 fw-bold'>$row[PName]</h5>
       <p class='card-text text-danger fs-4 fw-bold'>Rs: $row[PPrice]</p>
-      <input type='number' value = 'min='1' max= '20'' placeholder = 'Quantity'> <br><br>
-      <input type='submit' class='btn btn-danger text-white w-100' value = 'Add To Cart'>
+      <input type = 'hidden' name = 'PName' value= '$row[PName]'>
+      <input type = 'hidden' name = 'PPrice' value= '$row[PPrice]'>
+      <input type='number' name = 'PQuantity' value =' min='1' max= '10'' placeholder = 'Quantity'> <br><br>
+      <input type='submit' name = 'addCart' class='btn btn-danger text-white w-100' value = 'Add To Cart'>
     </div>
    </div>  
+   </form>
   </div>
     ";
    }
---
+++ user/viewCart.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>viewCart</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" 
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <?php
+      include 'header.php';
+    ?>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12 text-center bg-light mb-5 rounded">
+            <h1 class="text-dark">My Cart</h1>
+        </div>
+    </div>
+</div>   
+
+
+<div class="container-fluid">
+    <div class="row justify-content-around" >
+        <div class="col-sm-12 col-md-6 col-lg-9">
+            <table class="table table-bordered text-center">
+                <thead class="bg-danger text-dark fs-5">
+                    <th>index no.</th>
+                    <th>Product Name</th>
+                    <th>Product Price</th>
+                    <th>Product Quantity</th>
+                    <th>Total Price</th>
+                    <th>Update</th>
+                    <th>Delete</th>
+                </thead>
+
+                <tbody>
+                    <?php
+                        session_start();
+                        $total = 0;
+                        if(isset($_SESSION['cart'])){
+                            foreach($_SESSION['cart'] as $key => $value){
+                                $total = $value['productPrice'] * $value['productQuantity'];
+                                echo "
+                                <form action = 'Insertcart.php' method= 'POST'>
+                                <tr>
+                                 <td>$key</td>
+                                 <td>$value[productName]</td>
+                                 <td>$value[productPrice]</td>
+                                 <td>$value[productQuantity]</td>
+                                 <td>$total</td>
+                                 <td><button class= 'btn-warning'>Update</button></td>
+                                 <td><button name= 'remove' class= 'btn-danger'>Delete</button></td>
+                                 <td> <input type='text' name='item' value= '$value[productName]'> </td>
+                                </tr>
+                                </form>
+                                ";
+
+                            }
+                        }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+</body>
+</html>
