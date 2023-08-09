2ef57e9a367b21f1593bf3f153a1f988c65f7450
Saman Kumara <samankumararajapaksha660@gmail.com>
2023-08-09 11:49:43 +0530

    modified

---
+++ user/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" 
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="userstyle.css">
+</head>
+<body>
+
+
+<nav class="navbar navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand"><img src="logo.jpeg" class="logo" alt=""></a>
+    </div>
+    <div class="d-flex">
+    <a href="" class="text-secondary text-decoration-none pe-2"><i class="fas fa-home"></i>Home</a>
+    <a href="" class="text-secondary text-decoration-none pe-2"><i class="fas fa-shopping-cart"></i>Cart(0) |</a>
+
+    <span  class="text-dark-gray pe-2">
+       <i class="fas fa-user-shield"></i>Hello,
+       <a href="" class="text-secondary text-decoration-none pe-2">Login</a>
+       <a href="" class="text-secondary text-decoration-none pe-2">Admin</a>
+    </span>
+    </div>
+</nav>
+</body>
+</html>
