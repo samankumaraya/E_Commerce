8e14d97049bfc4b6ff759cff0aa9d7612ed20859
Saman Kumara <samankumararajapaksha660@gmail.com>
2023-08-12 23:40:17 +0530

    add usr register

---
+++ user/form/insert.php
@@ -0,0 +1,44 @@
+<?php
+
+$Con = mysqli_connect('localhost','root','','ecommerce');
+
+if(isset($_POST['submit'])){
+    $Name = $_POST['name'];
+    $Email = $_POST['email']; 
+    $Number = $_POST['number']; 
+    $Password = $_POST['password']; 
+
+    
+     $Dup_Email = mysqli_query($Con,"SELECT * FROM `users` WHERE Email = '$Email'");
+     $Dup_username = mysqli_query($Con,"SELECT * FROM `users` WHERE username  = '$Name'");
+
+     if(mysqli_num_rows($Dup_Email)){
+        echo "
+         <script>
+           alert('This Email Already has registered');
+           window.location.href = 'register.php';
+         </script>
+        ";
+     }
+     if(mysqli_num_rows($Dup_username)){
+        echo "
+         <script>
+           alert('This username has already taken');
+           window.location.href = 'register.php';
+         </script>
+        ";
+     }
+     else{
+        mysqli_query($Con, "INSERT INTO `users`(`username`, `Email`, `Number`, `Password`) 
+        VALUES ('$Name','$Email','$Number','$Password')");
+        echo "
+        <script>
+          alert('Registered success');
+          window.location.href = 'register.php';
+        </script>
+       ";
+     }
+
+
+}
+?>
